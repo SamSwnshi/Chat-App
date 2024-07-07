8618479aff3128ffe70053dcf27bc842bed46f07
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2024-07-07 14:27:07 +0530

    Deployed at Render

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
   useEffect(() => {
     if (auth) {
       //NOTE - here we connect the backend to frontend !
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-app-if2l.onrender.com", {
         query: {
           userId: auth._id,
         },
